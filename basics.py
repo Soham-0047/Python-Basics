167bdd265a64ef98c4b2fba9cc68fabbb402cc64
Soham-0047 <solutionnoton@gmail.com>
2026-03-09 20:32:36 +0530

    feat: tutorial progress

--- basics.py
+++ basics.py
@@ -60,6 +60,7 @@
 # Reference counting, Cyclic garbage collector
 
 import sys;
+import math;
 
 # x =  [1,2,3]; #1
 # y = x;
@@ -86,3 +87,37 @@ import sys;
 # del b.next
 # print(sys.getrefcount(a), sys.getrefcount(b))
 # print(0.2 + 0.4);
+
+# print(math.inf,math.nan, math.isnan(math.nan));
+
+# a =  323;
+# b = 2332;
+# print(id(a),id(b))
+
+# is_list = [12,12,22,44,444,444,44,44,55]
+# print(set(is_list))
+# print(tuple(is_list))
+# print(list(set(is_list)))
+
+# list_join = ["A","B","C","D"]
+
+# print('345'.join(list_join))
+
+# st = "In A BMW Car"
+
+# print(st.find("In"),st.split(),st.capitalize(),st.count("I"))
+
+# Number operations
+
+# x = 20
+# print(x//5, x**3)
+
+
+# So basically we  will craerte list
+# list_container = [];
+# for i in range(1,5):
+#     list_container.append(i ** 2)
+# print(list_container)
+
+# num_range = [i for i in range(1,500) if(i%3 == 0)]
+# print(num_range)
